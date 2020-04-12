87044914bc3c7c5e0eb146a8e7fe97050be2b50d
Genki Furukawa <genki.furukawa@gmail.com>
2020-04-13 00:45:20 +0900

    [add] 実行サンプルを追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 console.log("Hello TypeScript!")
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
